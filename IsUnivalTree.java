dad506ec6dc8334600fa5663263af0395e9beab7
Phillip Bay <35547055+Steelyphil1@users.noreply.github.com>
2020-10-16 14:48:35 -0700

    Add files via upload

---
+++ IsUnivalTree.java
@@ -0,0 +1,64 @@
+/*
+    Given the root to a binary tree, count the number of unival subtrees.
+ */
+
+public class IsUnivalTree {
+
+    public static void main (String[] args) {
+        Node root = new Node(0, null, null);
+        Node node1 = new Node(1,null,null);
+        Node node2 = new Node(0,null,null);
+        Node node3 = new Node(1,null,null);
+        Node node4 = new Node(0,null,null);
+        Node node5 = new Node(1,null,null);
+        Node node6 = new Node(1,null,null);
+
+        root.setLeft(node1);
+        root.setRight(node2);
+        node2.setLeft(node3);
+        node2.setRight(node4);
+        node3.setLeft(node5);
+        node3.setRight(node6);
+
+        System.out.println(isUnival(root));
+        System.out.println(isUnival(node3));
+    }
+    public static boolean isUnival(Node root) {
+        if(root.left == null && root.right == null) {
+            return true;
+        }
+        if(root.left != null && root.left.value != root.value) {
+            return false;
+        }
+        if(root.right != null && root.right.value != root.value) {
+            return false;
+        }
+        if(isUnival(root.left) && isUnival(root.right)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static class Node {
+        private int value;
+        private Node left;
+        private Node right;
+        public Node(int value, Node left, Node right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+        public Node getLeft() {
+            return this.left;
+        }
+        public Node getRight() {
+            return this.right;
+        }
+        public void setLeft(Node n) {
+            this.left = n;
+        }
+        public void setRight(Node n) {
+            this.right = n;
+        }
+    }
+}
